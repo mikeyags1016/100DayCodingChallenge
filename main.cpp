fd0b5af7aae4c92929a61189fae983a2b1f9451e
mikeyags1016 <44143795+mikeyags1016@users.noreply.github.com>
2018-10-16 14:52:07 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,83 +1,92 @@
+//
+//  main.cpp
+//  list sorter
+//
+//  Created by Michael A on 10/16/18.
+//  Copyright © 2018 Michael A. All rights reserved.
+//
+
 #include <iostream>
+#include <random>
+#include <time.h>
 using namespace std;
 
-float addition(float x, float y)
+int arr[20000];
+
+void swap(int *i, int *j)
 {
-    return x + y;
+    int temp = *i;
+    *i = *j;
+    *j = temp;
 }
 
-float subtraction(float x, float y)
+void bubble_sort(int *list, int size)
 {
-    return x - y;
-}
-
-float multiplication(float x, float y)
-{
-    return x * y;
-}
-
-float division(float x, float y)
-{
-    if (y == 0)
+    //Loop through entire list
+    for (int i = 0; i < size - 1; i++)
     {
-        throw "0 Division";
-    }
-    return x / y;
-}
-
-float exponent(float x, float y)
-{
-    float sum = 1;
-    for (int i = 0; i < y; i++)
-    {
-        sum *= x;
-    }
-    return sum;
-}
-
-void equation_definer(char operators, float x, float y)
-{
-    switch (operators)
-    {
-        case '+':
-            cout << addition(x, y) << endl;
-            break;
-        case '-':
-            cout << subtraction(x, y) << endl;;
-            break;
-        case '*':
-            cout << multiplication(x, y) << endl;;
-            break;
-        case '/':
-            try
+        //For each element, loop through the rest of the elements
+        for (int j = 0; j < size - i - 1; j++)
         {
-            cout << division(x, y) << endl;
+            //If j is bigger than the next element, swap
+            if (list[j] > list[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+            }
         }
-            catch (const char* message)
-        {
-            cerr << message << endl;
-        }
-            break;
-        case '^':
-            cout << exponent(x, y) << endl;
-            break;
-        default:
-            throw string("Invalid Input");
-            break;
     }
 }
 
-int main(int argc, const char * argv[])
+void selection_sort(int *list, int size)
 {
-    float x = 0, y = 0;
-    char operator_sign;
+    //Loop through entire list
+    for (int i = 0; i < size - 1; i++)
+    {
+        //Set current element to i
+        int smallest = i;
+        
+        //Loop through rest of list
+        for (int j = i + 1; j < size; j++)
+        {
+            //If current element is smaller than the current smallest,
+            //set current to smallest
+            if (list[j] < list[smallest])
+            {
+                smallest = j;
+            }
+            //When you have gone through the entire list,
+            //swap the old smallest (i) with the current smallest
+            //if they aren't equal
+            if (smallest != i)
+            {
+                swap(&list[smallest], &list[i]);
+            }
+        }
+    }
+}
+
+void print(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << ", ";
+    }
+    cout << endl;
+}
+
+int main(int argc, const char * argv[]) {
+    int n = (sizeof(arr)/sizeof(*arr));
     
-    cout << "Enter a number: ";
-    cin >> x;
-    cout << endl << "Enter another number: ";
-    cin >> y;
-    cout << endl << "Enter your operator: ";
-    cin >> operator_sign;
+    for (int i = 0; i < 20000; i++)
+    {
+        arr[i] = rand() % 100 + 1;
+    }
     
-    equation_definer(operator_sign, x, y);
+    time_t start = time(0);
+    
+    selection_sort(arr, n);
+    print(arr, n);
+    
+    double seconds_since_start = difftime( time(0), start);
+    cout << seconds_since_start << " s" << endl;
 }
