af8c7f913eedf0aa989c891ac03e6561b0eb9a7a
mikeyags1016 <44143795+mikeyags1016@users.noreply.github.com>
2018-10-18 11:28:05 -0600

    Add files via upload

--- test_list_sorter.py
+++ test_list_sorter.py
@@ -2,18 +2,34 @@ import unittest
 import list_sorter
 
 
+
 class TestListSorter(unittest.TestCase):
+
+    def setUp(cls):
+        cls.unsorted_list1 = [50,40,10,30,24]
+        cls.unsorted_list2 = [50, -50, 50, 50, 40, 10, 30, 24]
+
+        cls.sorted_list1 = [10,24,30,40,50]
+        cls.sorted_list2 = [-50, 10, 24, 30, 40, 50, 50, 50]
+
+        cls.size1 = len(cls.unsorted_list1)
+        cls.size2 = len(cls.unsorted_list2)
+
     def test_bubble_sort(self):
-        self.assertEqual(list_sorter.bubble_sort([50,50,50,50,40,10,30,24]),
-                         [10,24,30,40,50,50,50,50])
+        self.assertEqual(list_sorter.bubble_sort(self.unsorted_list1),
+                         self.sorted_list1)
 
     def test_selection_sort(self):
-        self.assertEqual(list_sorter.bubble_sort([50, -50, 50, 50, 40, 10, 30, 24]),
-                         [-50, 10, 24, 30, 40, 50, 50, 50])
+        self.assertEqual(list_sorter.bubble_sort(self.unsorted_list2),
+                         self.sorted_list2)
 
     def test_insertion_sort(self):
-        self.assertEqual(list_sorter.insertion_sort([4,3,17,44,31,51,26]),
-                         [3,4,17,26,31,44,51])
+        self.assertEqual(list_sorter.insertion_sort(self.unsorted_list1),
+                         self.unsorted_list1)
+        
+    def test_merge_sort(self):
+        self.assertEqual(list_sorter.merge_sort(self.unsorted_list2),
+                         self.sorted_list2)
 
 
 if __name__ == "__main__":
